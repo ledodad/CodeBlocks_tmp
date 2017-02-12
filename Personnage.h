e36c9340bb315d4573104fa026f30ccb978d2f46
ledodad <ledodad@gmx.com>
2017-02-12 18:10:38 +0100

    first

---
+++ Personnage.h
@@ -0,0 +1,26 @@
+#ifndef PERSONNAGE_H_INCLUDED
+#define PERSONNAGE_H_INCLUDED
+
+#include <string>
+
+class Personnage
+
+{
+public:
+
+    void recevoirDegats(int nbDegats);
+    void attaquer(Personnage &Cible);
+    void boirPotionDeVie(int quantitePotion);
+    void changerArme(std::string nomNouvelleArme, degatsArme);
+    bool estVivant();
+
+private:
+    int m_vie;
+    int m_mana;
+    std::string m_nomArme;
+    int m_degatsArme;
+
+};
+
+
+#endif // PERSONNAGE_H_INCLUDED
