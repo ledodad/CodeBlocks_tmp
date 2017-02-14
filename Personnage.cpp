40a5ece00cc04fe0deec99de89bc9e708e531cb2
ledodad <ledodad@gmx.com>
2017-02-14 01:18:27 +0100

    uptate

--- Personnage.cpp
+++ Personnage.cpp
@@ -7,10 +7,23 @@
 
 using namespace std;
 
-    void recevoirDegats(int nbDegats);
-    void attaquer(Personnage &Cible);
-    void boirPotionDeVie(int quantitePotion);
-    void changerArme(string nomNouvelleArme, degatsArme);
-    bool estVivant();
+    void recevoirDegats(int nbDegats)
+    {
+        m_vie-=nbDegats;
+
+        if (m_vie<0)
+        {
+            m_vie=0;
+        }
+
+    }
+    void attaquer(Personnage &cible)
+    {
+        cible.recevoirDegats(m_degatsArme);
+    }
+
+    void boirPotionDeVie(int quantitePotion)
+    void changerArme(string nomNouvelleArme, degatsArme)
+    bool estVivant()
 
 #endif // PERSONNAGE_CPP_INCLUDED
